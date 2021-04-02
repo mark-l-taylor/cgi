890569b0134d368f854e46db07a3e82f2ee01496
Mark L Taylor <taylormarkld@gmail.com>
2021-04-02 11:21:38 -0700

    completed output variables for printing information

--- cgi-bin/cgi_2.py
+++ cgi-bin/cgi_2.py
@@ -23,10 +23,10 @@ body = """<html>
 </body>
 </html>""".format(
     software=os.environ.get('SERVER_SOFTWARE', default),
-    script='aaaa',
-    month='bbbb',
-    date='cccc',
-    year='dddd',
-    client_ip='eeee'
+    script=os.environ.get('SCRIPT_NAME', default),
+    month=datetime.datetime.now().strftime('%B'),
+    date=datetime.date.today().day,
+    year=datetime.date.today().year,
+    client_ip=os.environ.get('REMOTE_ADDR', default)
 )
 print(body)
